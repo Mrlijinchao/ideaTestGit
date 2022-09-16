b8e65f2280e9e80e9a5658c7d50ce055616fd7f2
Mrlijinchao <2116639781@qq.com>
2022-09-16 23:12:59 +0800

    Test.java modied

--- src/main/java/com/mrli/Test.java
+++ src/main/java/com/mrli/Test.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Configuration
 public class Test {
+    private String id;
     public static void main(String[] args) {
         System.out.println("你好帅啊！");
     }
