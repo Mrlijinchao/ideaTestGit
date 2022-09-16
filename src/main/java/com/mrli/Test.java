99f850fa963e5711b2e7dfabcc1de290dd52009e
Mrlijinchao <2116639781@qq.com>
2022-09-16 22:54:40 +0800

    Test.java modied

--- src/main/java/com/mrli/Test.java
+++ src/main/java/com/mrli/Test.java
@@ -1,5 +1,8 @@
 package com.mrli;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Test {
     public static void main(String[] args) {
         System.out.println();
