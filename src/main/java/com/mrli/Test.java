84520aa51b037a60c4f05066cf77aa468c060fe6
Mrlijinchao <2116639781@qq.com>
2022-09-16 22:55:39 +0800

    Test.java modied

--- src/main/java/com/mrli/Test.java
+++ src/main/java/com/mrli/Test.java
@@ -1,7 +1,9 @@
 package com.mrli;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+@Component
 @Configuration
 public class Test {
     public static void main(String[] args) {
